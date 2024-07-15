3edd716aad01ee6afc1e94c93a442f9af0ab3bc3
chrappah <chrappahkwasi@gmail.com>
2024-07-15 16:31:18 +0000

    update_docker

--- pages/4_Models.py
+++ pages/4_Models.py
@@ -1,11 +1,7 @@
 # import streamlit as st
 # import requests
 # import pandas as pd
-# import joblib
-# import os
-# import datetime
-# import yaml
-# from yaml.loader import SafeLoader
+
 
 # #Define the backend url
 # backend_url = 'http://127.0.0.1:8000'
